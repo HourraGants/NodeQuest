2b58ebe58ffa6e8adeef9d3f588d090a8112d199
unknown <arthur.saussereau@hotmail.fr>
2024-12-06 14:24:48 +0100

    Added the Cowsay module

---
+++ index.js
@@ -0,0 +1,11 @@
+// server.js
+import cowsay from "cowsay";
+import "dotenv/config";
+
+console.log(
+	cowsay.say({
+		text: `My name is ${process.env.NAME}, my campus is ${process.env.CAMPUS}`,
+		e: "oO",
+		T: "U ",
+	}),
+);
